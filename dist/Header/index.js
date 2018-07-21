f79a33b893a7396cea6037d76191b48a0a578d66
René Viering <rene.viering@gmail.com>
2018-07-22 01:07:03 +0200

    Update component library.

---
+++ dist/Header/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Header = function Header() {
+  return _react2.default.createElement(
+    'header',
+    null,
+    'I am the header\u2026'
+  );
+};
+
+exports.default = Header;
---
+++ dist/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.Header = undefined;
+
+var _Header = require('./components/Header');
+
+var _Header2 = _interopRequireDefault(_Header);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = {
+	Header: _Header2.default
+};
+exports.Header = _Header2.default;
