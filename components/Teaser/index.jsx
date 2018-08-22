01b4ba9c93a1e2a65c699786badd2216377f87fd
René Viering <rene.viering@gmail.com>
2018-08-23 01:36:44 +0200

    Fix alignment of teaser texts and image avatar size.

--- components/Teaser/index.jsx
+++ components/Teaser/index.jsx
@@ -15,22 +15,32 @@ const styles = {
     alignItems: 'center',
     flexDirection: 'column',
     color: 'white',
-    paddingTop: 60
+    paddingTop: 80
+  },
+  Headline: {
+    marginBottom: 0
+  },
+  PersonInfo: {
+    textAlign: 'center'
   },
   Avatar: {
-    width: 250,
-    height: 250,
-    maxWidth: '50vw',
-    maxHeight: '50vw',
-    borderRadius: '50%'
+    width: '40vh',
+    height: '40vh',
+    maxWidth: '180px',
+    maxHeight: '180px',
+    borderRadius: '50%',
+    '@media(min-width: 700px)': {
+      maxWidth: '250px',
+      maxHeight: '250px',
+    }
   }
 }
 
 const Teaser = ({ classes }) => (
   <header className={ classes.Teaser }>
     <img className={ classes.Avatar } src={ avatar } alt="Avatar René Viering" />
-    <h1>René Viering</h1>
-    <p>
+    <h1 className={ classes.Headline }>René Viering</h1>
+    <p className={ classes.PersonInfo }>
       JavaScript Freelancer<br />
       <small>Kassel, Germany</small>
     </p>
